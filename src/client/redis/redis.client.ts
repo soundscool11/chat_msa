ef69ef66352147c39e4cf246a6eb68fbbfc75dbc
moo-co <code.pentagon@gmail.com>
2023-07-29 12:17:42 +0900

    redis 연결 제거

--- src/client/redis/redis.client.ts
+++ src/client/redis/redis.client.ts
@@ -7,35 +7,31 @@ export class RedisInstance {
   subClient: RedisClientType;
 
   constructor() {
-    this.client = createClient({
-      url: `${process.env.REDIS_HOST}`,
-      password: `${process.env.REDIS_PW}`,
-      socket: {
-        connectTimeout: 60000,
-      },
-    });
-
-    this.client.on('error', (err) => {
-      console.log('redis error ', err);
-    });
-
-    this.client.connect().then(() => {
-      console.log('Connect to redis done');
-    });
-    this.subClient = this.client.duplicate();
-
-    process.on('exit', () => {
-      this.disconnect();
-    });
-
-    process.on('SIGINT', () => {
-      this.disconnect();
-      process.exit(2);
-    });
+    // this.client = createClient({
+    //   url: `${process.env.REDIS_HOST}`,
+    //   password: `${process.env.REDIS_PW}`,
+    //   socket: {
+    //     connectTimeout: 60000,
+    //   },
+    // });
+    // this.client.on('error', (err) => {
+    //   console.log('redis error ', err);
+    // });
+    // this.client.connect().then(() => {
+    //   console.log('Connect to redis done');
+    // });
+    // this.subClient = this.client.duplicate();
+    // process.on('exit', () => {
+    //   this.disconnect();
+    // });
+    // process.on('SIGINT', () => {
+    //   this.disconnect();
+    //   process.exit(2);
+    // });
   }
 
   disconnect() {
-    console.log('disconnect');
-    this.client.quit();
+    // console.log('disconnect');
+    // this.client.quit();
   }
 }
